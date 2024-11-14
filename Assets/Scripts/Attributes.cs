cd3733b82e0564b34fb2ff28f888798db097143f
Emperor Bocky <Kenworthy.K01@elliotthudsoncollege.ac.uk>
2024-11-14 21:26:48 +0000

    Attributes 1
    setup attributes

---
+++ Assets/Scripts/Attributes.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attributes {
+	public int strength { get { return strength; } set { strength = Mathf.Clamp(value, 0, 100); } }
+	public int fortitude = 0;
+	public int agility = 0;
+	public int intelligence = 0;
+	public int charisma = 0;
+
+	private int Clamp(string statName, int stat) {
+		int total = 0;
+		total += this.strength;
+		total += this.fortitude;
+		total += this.agility;
+		total += this.intelligence;
+		total += this.charisma;
+
+		return stat;
+	}
+
+}

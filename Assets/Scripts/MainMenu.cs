3eefbc3eb5bc117e01330d7a60ac9932789f9974
Kaden M Kenworthy <Kenworthy.K01@elliotthudsoncollege.ac.uk>
2024-12-19 11:58:18 +0000

    Menu 2 (sprint 3)
    Finished menu design and functionality.

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,20 +1,46 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour {
 
+	private GameObject controlScrn;
+	private GameObject creditScrn;
+
+	private bool controlsOpen = false;
+	private bool creditsOpen = false;
+
+	private void Start() {
+		// Get credits and controls frames
+		controlScrn = transform.GetChild(5).gameObject;
+		creditScrn = transform.GetChild(6).gameObject;
+	}
 
 	public void OnPlayBtnClicked() {
-		
+		SceneManager.LoadScene("DungeonLevel1");
 	}
 
 	public void OnControlsBtnClicked() {
-		
+		// Toggle visibility of control frame
+		if (controlsOpen) {
+			controlsOpen = false;
+			controlScrn.transform.position = new Vector3(-1000, 0, 0) + transform.position;
+		} else {
+			controlsOpen = true;
+			controlScrn.transform.position = new Vector3(-400, 0, 0) + transform.position;
+		}
 	}
 
 	public void OnCreditsBtnClicked() {
-		
+		// Toggle visibility of credits frame
+		if (creditsOpen) {
+			creditsOpen = false;
+			creditScrn.transform.position = new Vector3(1000, 0, 0) + transform.position;
+		} else {
+			creditsOpen = true;
+			creditScrn.transform.position = new Vector3(400, 0, 0) + transform.position;
+		}
 	}
 
 }

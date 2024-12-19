ff842c108e23274f5b35df0debfe7880bd03fa65
Kaden M Kenworthy <Kenworthy.K01@elliotthudsoncollege.ac.uk>
2024-12-19 10:50:53 +0000

    Attacks 3 / Menu 1
    Added main menu and attack usage

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour {
+
+
+	public void OnPlayBtnClicked() {
+		
+	}
+
+	public void OnControlsBtnClicked() {
+		
+	}
+
+	public void OnCreditsBtnClicked() {
+		
+	}
+
+}
